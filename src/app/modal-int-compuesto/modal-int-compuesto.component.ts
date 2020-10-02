b4a869f1e783c73c971e5e0f32f528c7cfc8bcc7
Junior <juniorjerameel1519@gmail.com>
2020-10-01 20:30:59 -0700

    se programo el interes compuesto

--- src/app/modal-int-compuesto/modal-int-compuesto.component.ts
+++ src/app/modal-int-compuesto/modal-int-compuesto.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -8,8 +8,15 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./modal-int-compuesto.component.scss'],
 })
 export class ModalIntCompuestoComponent implements OnInit {
+  myForm: FormGroup;
+  options:string;
 
-  constructor(
+  cn:number;
+  co:number;
+  i:number;
+  n: number;
+
+  constructor (
     public modalController: ModalController,
     private fb: FormBuilder
   ) {}
@@ -18,6 +25,42 @@ export class ModalIntCompuestoComponent implements OnInit {
     this.modalController.dismiss();
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.myForm = this.fb.group ({
+      capital: null,
+      tazaInteres: null,
+      tiempo: null,
+      valorFinal: null,
+    });
+
+    this.myForm.valueChanges.subscribe(console.log);
+  }
+
+  calcularValorFinal() {
+    const v = this.myForm.value;
+    var base = 1 + v.tazaInteres;
+
+    this.cn = v.capital *  Math.pow( base, v.tiempo);
+  }
+
+  calcularCapital() {
+    const v = this.myForm.value;
+    var base = 1 + v.tazaInteres;
+
+    this.co = v.valorFinal / (Math.pow( base, v.tiempo));
+  }
+
+  calcularTiempo() {
+    const v = this.myForm.value;
+    var base = 1 + v.tazaInteres;
+
+    this.n = Math.log(v.valorFinal / v.capital) / Math.log(base);
+  }
+
+  calcularTaza(){
+    const v = this.myForm.value;
+
+    this.i = Math.pow(v.tiempo, v.valorFinal/v.capital) -1;
+  }
 
 }
